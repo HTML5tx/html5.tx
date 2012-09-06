6c5eb3ff36556d111cf32817eea314698ae83b5c
Matt Carver <carver520@gmail.com>
2012-09-06 09:30:41 -0400

    responsive image js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,36 @@
 (function($){
-	
-// The DOM is now ready
+
+// RESPONSIVE IMAGES
+
+	var viewportWidth = $(window).width();
+	var small = 600;
+	var medium = 992;
+	var large = 1280;
+
+	if(viewportWidth > small && viewportWidth < medium){
+		var img = $('img[data-m-src]');
+
+		img.each(function(){
+			var mediumImg = $(this).attr('data-m-src');
+			$(this).attr('src', mediumImg);
+		});
+
+	}if(viewportWidth > medium && viewportWidth < large){
+		var img = $('img[data-l-src]');
+
+		img.each(function(){
+			var mediumImg = $(this).attr('data-l-src');
+			$(this).attr('src', mediumImg);
+		});
+
+	}else{
+ 
+	}
+
+	console.log(viewportWidth);
 
 
-$(function() {
+// END RESPONSIVE IMAGES
 
-});
 
 })( jQuery.noConflict() ); // Pass in jQuery so we can safely use the $ alias within this block
